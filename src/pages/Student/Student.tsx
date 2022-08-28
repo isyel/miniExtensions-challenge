50007706e4e80ea17233eefb57d5c0f52e14c892
isyel <isykaal4saviour@gmail.com>
2022-08-28 18:51:19 +0100

    add styles

--- src/pages/Student/Student.tsx
+++ src/pages/Student/Student.tsx
@@ -1,31 +1,46 @@
 import React, { useEffect } from "react";
 import { initialStateT } from "@/redux/reducers/initialState.types";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getStudents } from "./../../api/studentsApi";
+
+import styles from "./Student.module.css";
+import { getAllStudentsSuccess } from "./../../redux/actions/students.actions";
+import ClassesList from "./../../components/ClassesList";
 
 const Student = () => {
   const params = useParams();
   const classes = useSelector((state: initialStateT) => state.classes.classes);
 
+  const dispatch = useDispatch();
+
   useEffect(() => {
     // getClasses();
     if (classes) {
       const filterFormulaString = classes
-        .map((singleClass) => `FIND('${singleClass.Name}',{Classes})`)
+        .map((singleClass) => `FIND('${singleClass.fields?.Name}',{Classes})`)
         .join(",");
       loadStudentsData(filterFormulaString);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [classes]);
 
   const loadStudentsData = async (filterFormulaString: string) => {
     const students = await getStudents(filterFormulaString);
-    console.log(students);
+    const mappedData = students.map((item) => ({
+      classes: item.fields.Classes as string[],
+      name: item.fields.Name as string,
+      id: item.id,
+    }));
+    dispatch(getAllStudentsSuccess(mappedData));
   };
 
-  console.log("classes in Student", classes);
-
-  return <div>Student</div>;
+  return (
+    <div className={styles.Student__container}>
+      <h2>Student: {params.name}</h2>
+      <ClassesList classes={classes} />
+    </div>
+  );
 };
 
 export default Student;
