2fd0bb3ef6dad1a15e9e10402b9b3db92f611a5c
isyel <isykaal4saviour@gmail.com>
2022-08-28 21:53:27 +0100

    update Api calls on name enter

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,27 +1,54 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { getAllClassesSuccess } from "../../redux/actions/classes.actions";
-import { getClasses } from "./../../api/classesApi";
+import { getClassesByIds } from "./../../api/classesApi";
 import { useNavigate } from "react-router-dom";
 import useDebounce from "./../../hooks/useDebounce";
 
 import styles from "./Home.module.css";
+import { getSingleStudent } from "./../../api/studentsApi";
+import { getStudentDetailsSuccess } from "./../../redux/actions/students.actions";
 
 const Home = () => {
   const [studentName, setStudentName] = useState<string>("");
+  const [error, setError] = useState<boolean>(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  useDebounce(() => handleGetClasses(), 2000, [studentName]);
+  useDebounce(() => handleGetStudent(), 1000, [studentName]);
 
-  const handleGetClasses = async () => {
-    const response = await getClasses(studentName);
-    const mappedData = response.map((item) => ({
-      fields: item.fields,
-      id: item.id,
-    }));
-    dispatch(getAllClassesSuccess(mappedData));
-    navigate(`/student/${studentName}`);
+  const handleGetStudent = async () => {
+    try {
+      const response = await getSingleStudent(studentName);
+      const mappedData = response.map((item) => ({
+        classes: item.fields.Classes as Array<string>,
+        name: item.fields.Name as string,
+        id: item.id,
+      }));
+      dispatch(getStudentDetailsSuccess(mappedData[0]));
+      handleGetClasses(mappedData[0].classes);
+    } catch (error) {
+      console.log("error", error);
+      setError(true);
+    }
+  };
+
+  const handleGetClasses = async (classes: Array<string>) => {
+    try {
+      const filterFormulaString = classes
+        .map((singleClass) => `FIND('${singleClass}',RECORD_ID())`)
+        .join(",");
+      const response = await getClassesByIds(filterFormulaString);
+      const mappedData = response.map((item) => ({
+        fields: item.fields,
+        id: item.id,
+      }));
+      dispatch(getAllClassesSuccess(mappedData));
+      navigate(`/student/${studentName}`);
+    } catch (error) {
+      console.log("error", error);
+      setError(true);
+    }
   };
 
   const handleStudentNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -36,6 +63,7 @@ const Home = () => {
         onChange={handleStudentNameChange}
         placeholder="Enter a student name"
       />
+      {error && <div className={styles.Home__error}>No student found</div>}
     </div>
   );
 };
