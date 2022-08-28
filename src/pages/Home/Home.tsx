bb68b8aa78481a0e39c51085e70ba2d2e100a17d
isyel <isykaal4saviour@gmail.com>
2022-08-28 17:51:02 +0100

    add classes and Home pages

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,27 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { getAllClassesSuccess } from "../../redux/actions/classes.actions";
+import { getClasses } from "./../../api/classesApi";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const [studentName, setStudentName] = useState<string>();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    handleGetClasses();
+  }, []);
+
+  const handleGetClasses = async () => {
+    const response = await getClasses("Jenny");
+    const mappedData = response.map((item) => item.fields);
+    console.log("mappedData", mappedData);
+
+    dispatch(getAllClassesSuccess(mappedData));
+    navigate(`/student/${studentName}`);
+  };
+
   return <div>Home</div>;
 };
 
--- src/pages/Student/Student.tsx
+++ src/pages/Student/Student.tsx
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { initialStateT } from "@/redux/reducers/initialState.types";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { getStudents } from "./../../api/studentsApi";
 
 const Student = () => {
+  const params = useParams();
+  const classes = useSelector((state: initialStateT) => state.classes.classes);
+
+  useEffect(() => {
+    // getClasses();
+    if (classes) {
+      const filterFormulaString = classes
+        .map((singleClass) => `FIND('${singleClass.Name}',{Classes})`)
+        .join(",");
+      loadStudentsData(filterFormulaString);
+    }
+  }, [classes]);
+
+  const loadStudentsData = async (filterFormulaString: string) => {
+    const students = await getStudents(filterFormulaString);
+    console.log(students);
+  };
+
+  console.log("classes in Student", classes);
+
   return <div>Student</div>;
 };
 
