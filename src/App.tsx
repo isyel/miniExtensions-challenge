f88389852fb8d9c395b02d1bf6cacc2b443de674
isyel <isykaal4saviour@gmail.com>
2022-08-28 17:49:40 +0100

    remove unnecessay code and files

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import { useEffect } from "react";
-import "./App.css";
-import { getStudents } from "./api/studentsApi";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import PageNotFound from "./pages/PageNotFound";
 import Student from "./pages/Student";
 import Home from "./pages/Home";
 
 function App() {
-  useEffect(() => {
-    // getClasses();
-    loadStudentsData();
-  }, []);
-
-  const loadStudentsData = async () => {
-    const students = await getStudents();
-    console.log(students);
-  };
-
   return (
     <div className="App">
       <Router>
