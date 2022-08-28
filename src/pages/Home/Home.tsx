13330e523cdab4da2ea2c476b9bbaa185667baa4
isyel <isykaal4saviour@gmail.com>
2022-08-28 18:14:03 +0100

    add styling and debounce functions

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,28 +1,43 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { getAllClassesSuccess } from "../../redux/actions/classes.actions";
 import { getClasses } from "./../../api/classesApi";
 import { useNavigate } from "react-router-dom";
+import useDebounce from "./../../hooks/useDebounce";
+
+import styles from "./Home.module.css";
 
 const Home = () => {
-  const [studentName, setStudentName] = useState<string>();
+  const [studentName, setStudentName] = useState<string>("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  useEffect(() => {
-    handleGetClasses();
-  }, []);
+  useDebounce(() => handleGetClasses(), 2000, [studentName]);
 
   const handleGetClasses = async () => {
-    const response = await getClasses("Jenny");
-    const mappedData = response.map((item) => item.fields);
-    console.log("mappedData", mappedData);
-
+    const response = await getClasses(studentName);
+    const mappedData = response.map((item) => ({
+      fields: item.fields,
+      id: item.id,
+    }));
     dispatch(getAllClassesSuccess(mappedData));
     navigate(`/student/${studentName}`);
   };
 
-  return <div>Home</div>;
+  const handleStudentNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setStudentName(e.target.value);
+  };
+
+  return (
+    <div className={styles.Home__container}>
+      <h1>Student Search</h1>
+      <input
+        type="text"
+        onChange={handleStudentNameChange}
+        placeholder="Enter a student name"
+      />
+    </div>
+  );
 };
 
 export default Home;
